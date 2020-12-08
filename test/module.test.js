522403788b8dda74c03494a6fc52969bdc59235d
gabor-recart <gabor.marosi@recart.com>
2020-12-08 19:25:30 +0100

    :sparkles: load recart-loader.js in the module

--- test/module.test.js
+++ test/module.test.js
@@ -5,23 +5,47 @@ const packageJSON = require('../package.json')
 const recartModule = require('../lib/module')
 
 describe('module', () => {
-  it('should add Recart plugin', () => {
-    const nuxtContext = {
+  let sandbox
+  before(() => {
+    sandbox = sinon.createSandbox()
+  })
+
+  let nuxtContext, moduleOptions
+  beforeEach(() => {
+    nuxtContext = {
       options: {
         nacelle: {
           abcd: 'some-nacelle-thingy'
-        }
+        },
+        head: { script: [] }
       },
-      addPlugin: sinon.stub()
+      addPlugin: sandbox.stub()
     }
 
-    const moduleOptions = {
-      efgh: 'some-module-thingy'
+    moduleOptions = {
+      recartSiteId: 'some-recart-siteid'
     }
+  })
 
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('should log error and exit if there is no Recart siteId', () => {
+    moduleOptions = {}
+    console.error = sandbox.spy()
     recartModule.call(nuxtContext, moduleOptions)
 
-    sinon.assert.calledOnceWithMatch(nuxtContext.addPlugin, params => {
+    sandbox.assert.calledOnceWithMatch(console.error, params => {
+      expect(params).to.equal('RecartSiteId is required on module options')
+      return true
+    })
+  })
+
+  it('should add Recart nacelle plugin', () => {
+    recartModule.call(nuxtContext, moduleOptions)
+
+    sandbox.assert.calledOnceWithMatch(nuxtContext.addPlugin, params => {
       expect(params.src).to.contain('recart-nacelle-plugin.js')
       expect(params.fileName).to.equal('recart-nacelle-plugin.js')
 
@@ -40,5 +64,17 @@ describe('module', () => {
 
   it('should export package.json as "meta"', () => {
     expect(recartModule.meta).to.equal(packageJSON)
+  })
+
+  it('should add recart-loader script', () => {
+    recartModule.call(nuxtContext, moduleOptions)
+
+    expect(nuxtContext.options.head.script.length).to.equal(1)
+    expect(nuxtContext.options.head.script[0]).to.eql({
+      hid: 'trackingjs',
+      src: `https://cdn.ghostmonitor.com/recart-loader.js?siteId=${moduleOptions.recartSiteId}&storefront=nacelle`,
+      async: true,
+      defer: true
+    })
   })
 })
