dd2a760e6a96710a14a20eb9d87264d75498f7b9
Dávid Naményi <dnamenyi@gmail.com>
2020-12-11 10:12:03 +0100

    👷 add webpack build (#10)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+module.exports = [
+  {
+    mode: 'production',
+    target: 'node',
+    entry: {
+      module: './lib/module.js'
+    },
+    output: {
+      filename: 'module.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    externals: [nodeExternals()]
+  },
+  {
+    mode: 'production',
+    target: 'node',
+    entry: {
+      plugin: './lib/recart-nacelle-plugin.js'
+    },
+    output: {
+      filename: 'recart-nacelle-plugin.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    externals: [nodeExternals()]
+  }
+]
